6d0ff7d09ef260e8a51dfa98cb325aa40b4c1a7d
Administrator <1260612638@qq.com>
2019-09-25 11:08:55 +0800

    maxs first commit

---
+++ src/main/java/com/maxs/MaxsApplication.java
@@ -0,0 +1,13 @@
+package com.maxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaxsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxsApplication.class, args);
+    }
+
+}
